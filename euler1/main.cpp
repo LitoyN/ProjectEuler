a13364e94c5d4b3216a75758f58adce1dea5dc45
cilloye <cilloye04@gmail.com>
2015-02-02 19:01:45 -0500

    Update 2/2
    Have working C++ solution containing two functions: one is iterative,
    Order(n); the other is Order(1).

--- euler1/main.cpp
+++ euler1/main.cpp
@@ -13,12 +13,9 @@ using namespace std;
 /*
  * 
  */
-int main(int argc, char** argv) {
-    
-    return 0;
-}
 
-int orderN(int n){
+
+void orderN(int n){
     int sum = 0;
     
     for(int i = 0; i < n; i++){
@@ -26,20 +23,31 @@ int orderN(int n){
             sum+=i;
     }
     
-    return sum;
+    cout << sum << endl;
 }
 
-int orderConstant(int n){
+void orderConstant(int n){
     
-    int maxMultThree = n-(n%3);
-    int maxMultFive = n-(n%5);
-    int maxMultFifteen = n-(n%15);
+    n -= 1;
+    int maxMultThree = n -(n%3);
+    int maxMultFive = n -(n%5);
+    int maxMultFifteen = n -(n%15);
     
     int numOfMultsThree = maxMultThree/3;
     int numOfMultsFive = maxMultFive/5;
     int numOfMultsFifteen = maxMultFifteen/15;
     
-    int sum = ((3*numOfMultsThree*(numOfMultsThree+1)) + (5*numOfMultsFive*(numOfMultsFive+1)) - (15*numOfMultsFifteen*(numOfMultsFifteen+1)))/2;
-    return sum;
+    int sum = ((3*numOfMultsThree*(numOfMultsThree+1)) +
+    (5*numOfMultsFive*(numOfMultsFive+1)) - (15*numOfMultsFifteen*(numOfMultsFifteen+1)))/2;
+    
+    cout << sum << endl;
+}
+
+int main(int argc, char** argv) {
+    
+    orderN(1000);
+    orderConstant(1000);
+    
+    return 0;
 }
 
